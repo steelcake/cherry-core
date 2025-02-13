8b383c9a0b2f13c876a3e76af59ee5a22f6d90ed
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2025-02-13 17:47:24 +0700

    save

--- svm-schema/src/lib.rs
+++ svm-schema/src/lib.rs
@@ -23,7 +23,7 @@ pub fn rewards_schema() -> Schema {
         Field::new("lamports", DataType::UInt64, true),
         Field::new("post_balance", DataType::UInt64, true),
         Field::new("reward_type", DataType::Utf8, true),
-        Field::new("commission", DataType::UInt64, true),
+        Field::new("commission", DataType::UInt8, true),
     ])
 }
 
@@ -108,7 +108,18 @@ pub fn instructions_schema() -> Schema {
         Field::new("transaction_index", DataType::UInt32, true),
         Field::new("instruction_address", DataType::List(Arc::new(Field::new("item", DataType::UInt32, true))), true),
         Field::new("program_id", DataType::Binary, true),
-        Field::new("accounts", DataType::List(Arc::new(Field::new("item", DataType::Binary, true))), true),
+        Field::new("a0", DataType::Binary, true),
+        Field::new("a1", DataType::Binary, true),
+        Field::new("a2", DataType::Binary, true),
+        Field::new("a3", DataType::Binary, true),
+        Field::new("a4", DataType::Binary, true),
+        Field::new("a5", DataType::Binary, true),
+        Field::new("a6", DataType::Binary, true),
+        Field::new("a7", DataType::Binary, true),
+        Field::new("a8", DataType::Binary, true),
+        Field::new("a9", DataType::Binary, true),
+        // accounts starting from index 10
+        Field::new("rest_of_accounts", DataType::List(Arc::new(Field::new("item", DataType::Binary, true))), true),
         Field::new("data", DataType::Binary, true),
         Field::new("d1", DataType::Binary, true),
         Field::new("d2", DataType::Binary, true),
@@ -119,4 +130,304 @@ pub fn instructions_schema() -> Schema {
         Field::new("is_committed", DataType::Boolean, true),
         Field::new("has_dropped_log_messages", DataType::Boolean, true),
     ])
+}
+
+#[derive(Default)]
+pub struct BlocksBuilder {
+    pub slot: builder::UInt64Builder,
+    pub hash: builder::BinaryBuilder,
+    pub parent_slot: builder::UInt64Builder,
+    pub parent_hash: builder::BinaryBuilder,
+    pub height: builder::UInt64Builder,
+    pub timestamp: builder::Int64Builder,
+}
+
+impl BlocksBuilder {
+    pub fn finish(mut self) -> RecordBatch {
+        RecordBatch::try_new(
+            Arc::new(blocks_schema()),
+            vec![
+                Arc::new(self.slot.finish()),
+                Arc::new(self.hash.finish()),
+                Arc::new(self.parent_slot.finish()),
+                Arc::new(self.parent_hash.finish()),
+                Arc::new(self.height.finish()),
+                Arc::new(self.timestamp.finish()),
+            ]
+        ).unwrap()
+    }
+}
+
+#[derive(Default)]
+pub struct RewardsBuilder {
+    pub block_slot: builder::UInt64Builder,
+    pub block_hash: builder::BinaryBuilder,
+    pub pubkey: builder::BinaryBuilder,
+    pub lamports: builder::UInt64Builder,
+    pub post_balance: builder::UInt64Builder,
+    pub reward_type: builder::StringBuilder,
+    pub commission: builder::UInt8Builder,
+}
+
+impl RewardsBuilder {
+    pub fn finish(mut self) -> RecordBatch {
+        RecordBatch::try_new(
+            Arc::new(rewards_schema()),
+            vec![
+                Arc::new(self.block_slot.finish()),
+                Arc::new(self.block_hash.finish()),
+                Arc::new(self.pubkey.finish()),
+                Arc::new(self.lamports.finish()),
+                Arc::new(self.post_balance.finish()),
+                Arc::new(self.reward_type.finish()),
+                Arc::new(self.commission.finish()),
+            ]
+        ).unwrap()
+    }
+}
+
+#[derive(Default)]
+pub struct TokenBalancesBuilder {
+    pub block_slot: builder::UInt64Builder,
+    pub block_hash: builder::BinaryBuilder,
+    pub transaction_index: builder::UInt32Builder,
+    pub account: builder::BinaryBuilder,
+    pub pre_mint: builder::BinaryBuilder,
+    pub post_mint: builder::BinaryBuilder,
+    pub pre_decimals: builder::UInt16Builder,
+    pub post_decimals: builder::UInt16Builder,
+    pub pre_program_id: builder::BinaryBuilder,
+    pub post_program_id: builder::BinaryBuilder,
+    pub pre_owner: builder::BinaryBuilder,
+    pub post_owner: builder::BinaryBuilder,
+    pub pre_amount: builder::UInt64Builder,
+    pub post_amount: builder::UInt64Builder,
+}
+
+impl TokenBalancesBuilder {
+    pub fn finish(mut self) -> RecordBatch {
+        RecordBatch::try_new(
+            Arc::new(token_balances_schema()),
+            vec![
+                Arc::new(self.block_slot.finish()),
+                Arc::new(self.block_hash.finish()),
+                Arc::new(self.transaction_index.finish()),
+                Arc::new(self.account.finish()),
+                Arc::new(self.pre_mint.finish()),
+                Arc::new(self.post_mint.finish()),
+                Arc::new(self.pre_decimals.finish()),
+                Arc::new(self.post_decimals.finish()),
+                Arc::new(self.pre_program_id.finish()),
+                Arc::new(self.post_program_id.finish()),
+                Arc::new(self.pre_owner.finish()),
+                Arc::new(self.post_owner.finish()),
+                Arc::new(self.pre_amount.finish()),
+                Arc::new(self.post_amount.finish()),
+            ]
+        ).unwrap()
+    }
+}
+
+#[derive(Default)]
+pub struct BalancesBuilder {
+    pub block_slot: builder::UInt64Builder,
+    pub block_hash: builder::BinaryBuilder,
+    pub transaction_index: builder::UInt32Builder,
+    pub account: builder::BinaryBuilder,
+    pub pre: builder::UInt64Builder,
+    pub post: builder::UInt64Builder,
+}
+
+impl BalancesBuilder {
+    pub fn finish(mut self) -> RecordBatch {
+        RecordBatch::try_new(
+            Arc::new(balances_schema()),
+            vec![
+                Arc::new(self.block_slot.finish()),
+                Arc::new(self.block_hash.finish()),
+                Arc::new(self.transaction_index.finish()),
+                Arc::new(self.account.finish()),
+                Arc::new(self.pre.finish()),
+                Arc::new(self.post.finish()),
+            ]
+        ).unwrap()
+    }
+}
+
+#[derive(Default)]
+pub struct LogsBuilder {
+    pub block_slot: builder::UInt64Builder,
+    pub block_hash: builder::BinaryBuilder,
+    pub transaction_index: builder::UInt32Builder,
+    pub log_index: builder::UInt32Builder,
+    pub instruction_address: builder::BinaryBuilder,
+    pub program_id: builder::BinaryBuilder,
+    pub kind: builder::StringBuilder,
+    pub message: builder::StringBuilder,
+}
+
+impl LogsBuilder {
+    pub fn finish(mut self) -> RecordBatch {
+        RecordBatch::try_new(
+            Arc::new(logs_schema()),
+            vec![
+                Arc::new(self.block_slot.finish()),
+                Arc::new(self.block_hash.finish()),
+                Arc::new(self.transaction_index.finish()),
+                Arc::new(self.log_index.finish()),
+                Arc::new(self.instruction_address.finish()),
+                Arc::new(self.program_id.finish()),
+                Arc::new(self.kind.finish()),
+                Arc::new(self.message.finish()),
+            ]
+        ).unwrap()
+    }
+}
+
+#[derive(Default)]
+pub struct TransactionsBuilder {
+    pub block_slot: builder::UInt64Builder,
+    pub block_hash: builder::BinaryBuilder,
+    pub transaction_index: builder::UInt32Builder,
+    pub version: builder::Int8Builder,
+    pub account_keys: builder::ListBuilder<builder::BinaryBuilder>,
+    pub address_table_lookups: AddressTableLookupsBuilder,
+    pub num_readonly_signed_accounts: builder::UInt32Builder,
+    pub num_readonly_unsigned_accounts: builder::UInt32Builder,
+    pub num_required_signatures: builder::UInt32Builder,
+    pub recent_blockhash: builder::BinaryBuilder,
+    pub signatures: builder::ListBuilder<builder::BinaryBuilder>,
+    pub err: builder::StringBuilder,
+    pub fee: builder::UInt64Builder,
+    pub compute_units_consumed: builder::UInt64Builder,
+    pub loaded_readonly_addresses: builder::ListBuilder<builder::BinaryBuilder>,
+    pub loaded_writeable_addresses: builder::ListBuilder<builder::BinaryBuilder>,
+    pub fee_payer: builder::BinaryBuilder,
+    pub has_dropped_log_messages: builder::BooleanBuilder,
+}
+
+pub struct AddressTableLookupsBuilder(pub builder::ListBuilder<builder::StructBuilder>);
+
+impl Default for AddressTableLookupsBuilder {
+    fn default() -> Self {
+        Self(builder::ListBuilder::new(builder::StructBuilder::new(
+            match address_table_lookup_dt() {
+                DataType::Struct(fields) => fields,
+                _ => unreachable!(),
+            },
+            vec![
+                Box::new(builder::BinaryBuilder::default()),
+                Box::new(builder::ListBuilder::new(builder::UInt64Builder::default())),
+                Box::new(builder::ListBuilder::new(builder::UInt64Builder::default())),
+            ],
+        )))
+    }
+}
+
+impl TransactionsBuilder {
+    pub fn finish(mut self) -> RecordBatch {
+        RecordBatch::try_new(
+            Arc::new(transactions_schema()),
+            vec![
+                Arc::new(self.block_slot.finish()),
+                Arc::new(self.block_hash.finish()),
+                Arc::new(self.transaction_index.finish()),
+                Arc::new(self.version.finish()),
+                Arc::new(self.account_keys.finish()),
+                Arc::new(self.address_table_lookups.0.finish()),
+                Arc::new(self.num_readonly_signed_accounts.finish()),
+                Arc::new(self.num_readonly_unsigned_accounts.finish()),
+                Arc::new(self.num_required_signatures.finish()),
+                Arc::new(self.recent_blockhash.finish()),
+                Arc::new(self.signatures.finish()),
+                Arc::new(self.err.finish()),
+                Arc::new(self.fee.finish()),
+                Arc::new(self.compute_units_consumed.finish()),
+                Arc::new(self.loaded_readonly_addresses.finish()),
+                Arc::new(self.loaded_writeable_addresses.finish()),
+                Arc::new(self.fee_payer.finish()),
+                Arc::new(self.has_dropped_log_messages.finish()),
+            ]
+        ).unwrap()
+    }
+}
+
+#[derive(Default)]
+pub struct InstructionsBuilder {
+    pub block_slot: builder::UInt64Builder,
+    pub block_hash: builder::BinaryBuilder,
+    pub transaction_index: builder::UInt32Builder,
+    pub instruction_address: builder::ListBuilder<builder::UInt32Builder>,
+    pub program_id: builder::BinaryBuilder,
+    pub a0: builder::BinaryBuilder,
+    pub a1: builder::BinaryBuilder,
+    pub a2: builder::BinaryBuilder,
+    pub a3: builder::BinaryBuilder,
+    pub a4: builder::BinaryBuilder,
+    pub a5: builder::BinaryBuilder,
+    pub a6: builder::BinaryBuilder,
+    pub a7: builder::BinaryBuilder,
+    pub a8: builder::BinaryBuilder,
+    pub a9: builder::BinaryBuilder,
+    pub rest_of_accounts: builder::ListBuilder<builder::BinaryBuilder>,
+    pub data: builder::BinaryBuilder,
+    pub d1: builder::BinaryBuilder,
+    pub d2: builder::BinaryBuilder,
+    pub d4: builder::BinaryBuilder,
+    pub d8: builder::BinaryBuilder,
+    pub error: builder::StringBuilder,
+    pub compute_units_consumed: builder::UInt64Builder,
+    pub is_committed: builder::BooleanBuilder,
+    pub has_dropped_log_messages: builder::BooleanBuilder,
+}
+
+impl InstructionsBuilder {
+    pub fn finish(mut self) -> RecordBatch {
+        RecordBatch::try_new(
+            Arc::new(instructions_schema()),
+            vec![
+                Arc::new(self.block_slot.finish()),
+                Arc::new(self.block_hash.finish()),
+                Arc::new(self.transaction_index.finish()),
+                Arc::new(self.instruction_address.finish()),
+                Arc::new(self.program_id.finish()),
+                Arc::new(self.a0.finish()),
+                Arc::new(self.a1.finish()),
+                Arc::new(self.a2.finish()),
+                Arc::new(self.a3.finish()),
+                Arc::new(self.a4.finish()),
+                Arc::new(self.a5.finish()),
+                Arc::new(self.a6.finish()),
+                Arc::new(self.a7.finish()),
+                Arc::new(self.a8.finish()),
+                Arc::new(self.a9.finish()),
+                Arc::new(self.rest_of_accounts.finish()),
+                Arc::new(self.data.finish()),
+                Arc::new(self.d1.finish()),
+                Arc::new(self.d2.finish()),
+                Arc::new(self.d4.finish()),
+                Arc::new(self.d8.finish()),
+                Arc::new(self.error.finish()),
+                Arc::new(self.compute_units_consumed.finish()),
+                Arc::new(self.is_committed.finish()),
+                Arc::new(self.has_dropped_log_messages.finish()),
+            ]
+        ).unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn smoke() {
+        BlocksBuilder::default().finish();
+        RewardsBuilder::default().finish();
+        TokenBalancesBuilder::default().finish();
+        BalancesBuilder::default().finish();
+        LogsBuilder::default().finish();
+        TransactionsBuilder::default().finish();
+        InstructionsBuilder::default().finish();
+    }
 }
