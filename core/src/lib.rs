938d2940b11fe116e28d4f263071a20ca2277370
yulesa <yulesa@gmail.com>
2025-04-07 16:35:21 -0300

    adding imports to cherry modules

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,4 +1,5 @@
 pub use cherry_cast as cast;
+pub use cherry_svm_decode as svm_decode;
 pub use cherry_evm_decode as evm_decode;
 pub use cherry_evm_schema as evm_schema;
 pub use cherry_evm_validate as evm_validate;
